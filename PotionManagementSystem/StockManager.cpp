be220637a6423dc57ddcb763e2c6ae5b70214db7
OptimalTime99 <optimal9744@gmail.com>
2025-12-23 10:48:50 +0900

    [🎉 add] 재고 관리 클래스 StockManager 파일 및 뼈대 작성

---
+++ PotionManagementSystem/StockManager.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include "StockManager.h"
+
+// 초기 재고 3개 추가 메서드 정의
+void StockManager::initializeStock(std::string potionName)
+{
+
+}
+
+// 특정 물약의 현재 재고 수량 반환 메서드 정의
+int StockManager::getStock(std::string potionName)
+{
+    return 1;
+}
+
+// 재고가 1개 이상인 경우 모험가에게 물약 지급 메서드 정의
+bool StockManager::dispensePotion(std::string potionName)
+{
+    return true;
+}
+
+// 모험가에게 지급한 물약의 공병 반환 받는 메서드 정의
+void StockManager::returnPotion(std::string potionName)
+{
+
+}
---
+++ PotionManagementSystem/StockManager.h
@@ -0,0 +1,29 @@
+﻿// StockManager.h
+
+#pragma once
+#include <map>
+#include <string>
+
+class StockManager
+{
+public:
+    
+    // 초기 재고 3개 추가 메서드 선언
+    void initializeStock(std::string potionName);
+
+    // 특정 물약의 현재 재고 수량 반환 메서드 선언
+    int getStock(std::string potionName);
+
+    // 재고가 1개 이상인 경우 모험가에게 물약 지급 메서드 선언
+    bool dispensePotion(std::string potionName);
+
+    // 모험가에게 지급한 물약의 공병 반환 받는 메서드 선언
+    void returnPotion(std::string potionName);
+
+private:
+    // 포션 재고
+    std::map <std::string, int> potionStock;
+
+    // 최대 포션 재고
+    const int MAX_STOCK = 3;
+};
